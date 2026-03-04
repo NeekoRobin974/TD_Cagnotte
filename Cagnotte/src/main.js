247109da4d68467fdc288de4c1fe14b749fa7114
Mattéo <matteo.cadet9@etu.univ-lorraine.fr>
2026-03-04 09:13:09 +0100

    exercice 4, mixin pour convertir les formats

--- Cagnotte/src/main.js
+++ Cagnotte/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import apiPlugin from './plugins/api'
+import tools from './plugins/tools'
 
 const app = createApp(App)
 
@@ -10,5 +11,6 @@ app.use(apiPlugin, {
   baseURL: 'https://donations.edu.netlor.fr',
   apiKey: import.meta.env.VITE_API_KEY
 })
+app.use(tools, { cutLength: 15 })
 
 app.mount('#app')
